dda21aa1edbb19d405bce6237564d6bf31ccd64b
Terry Zhao <terryzhao@runllama.ai>
2025-08-14 03:36:35 -0700

    add citation scroll

--- packages/ui/src/file-preview/index.ts
+++ packages/ui/src/file-preview/index.ts
@@ -3,4 +3,4 @@ export { PdfPreview } from "./pdf-preview";
 export { PdfNavigator } from "./pdf-navigator";
 export { useFileData } from "./use-file-data";
 export type { FilePreviewProps } from "./file-preview";
-export type { BoundingBox, FileData, PageData } from "./types";
+export type { BoundingBox, FileData, PageData, Highlight } from "./types";
