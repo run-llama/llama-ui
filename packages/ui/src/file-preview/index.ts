53e4f154e794ba2ed1ed73ae4c29c36f8b831bf7
Thuc Pham <thuc@lingble.com>
2025-09-26 17:19:09 +0700

    revert

--- packages/ui/src/file-preview/index.ts
+++ packages/ui/src/file-preview/index.ts
@@ -1,6 +1,5 @@
 export { FilePreview } from "./file-preview";
 export { PdfPreview } from "./pdf-preview";
-export { FilePreviewToolbar } from "./file-preview-toolbar";
 export { PdfNavigator } from "./pdf-navigator";
 export { useFileData } from "./use-file-data";
 export type { FilePreviewProps } from "./file-preview";
