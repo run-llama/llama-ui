8f012071498572530ac1fb6737ffad1d51e727e9
Terry Zhao <terry@runllama.ai>
2025-10-17 03:10:13 -0700

    fix

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
