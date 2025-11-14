19d0749d5c6f5e1ae7df8ff0b67e11d162c76b1c
Marcus Schiesser <mail@marcusschiesser.de>
2025-11-14 16:18:29 +0800

    fix: export document-preview globally

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,6 @@
 // Main entry point for @llamaindex/ui
 export * from "./file-upload";
+export * from "./document-preview";
 export * from "./extracted-data";
 export * from "./file-preview";
 export * from "./item-grid";
