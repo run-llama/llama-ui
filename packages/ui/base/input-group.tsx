ee8c435741dd990102d30cab84e2c9c52a5c85ff
leehuwuj <leehuwuj@gmail.com>
2026-02-20 10:46:49 +0700

    remove unused components

--- packages/ui/base/input-group.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type InputGroupProps = {
-  label?: string;
-  children: React.ReactNode;
-};
-
-export const InputGroup = (props: InputGroupProps) => {
-  return <div className="flex flex-col space-y-2">{props.children}</div>;
-};
